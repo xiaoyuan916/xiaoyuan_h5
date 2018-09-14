e69021da3e22f68c6e3f6a19555b222a1c111d39
xiaoyuan916 <402973804@qq.com>
2018-09-14 14:42:43 +0800

    提交elementUI的VUE组件的使用

---
+++ vueproject/vue_pro/vuepro_project/src/components/Typography.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <p>Typography字体</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Typography"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- vueproject/vue_pro/vuepro_project/src/router/index.js
+++ vueproject/vue_pro/vuepro_project/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import HomePage from '@/components/HomePage'
+import Typography from '@/components/Typography'
+import Icon from '@/components/Icon'
+import DropDown from '@/components/DropDown'
 
 Vue.use(Router)
 
@@ -10,6 +14,22 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    {
+      path: '/HomePage',
+      component: HomePage
+    },
+    {
+      path: '/Typography',
+      component: Typography
+    },
+    {
+      path: '/Icon',
+      component: Icon
+    },
+    {
+      path: '/DropDown',
+      component: DropDown
+    },
   ]
 })
