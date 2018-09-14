dc30f5996cc96e89558770593969cf27b9fa10a0
xiaoyuan916 <402973804@qq.com>
2018-09-14 14:55:24 +0800

    增加vuex的使用

--- vueproject/vue_pro/vuepro_project/src/main.js
+++ vueproject/vue_pro/vuepro_project/src/main.js
@@ -3,17 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Element  from 'element-ui';
+import Element from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import stores from './store/store'
 
 Vue.config.productionTip = false
 
-Vue.use(Element, { size: 'small', zIndex: 3000 });
+Vue.use(Element, {size: 'small', zIndex: 3000});
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store: stores,
   components: {App},
   template: '<App/>'
 })
